79d7b145284cc4e146bb522e3cd3d8f4c5145240
Hanjiseok1 <heemoon25@naver.com>
2022-07-25 22:22:08 +0900

    no message

---
+++ src/main/java/com/jiseok/todolist/service/todo/TodoService.java
@@ -0,0 +1,16 @@
+package com.jiseok.todolist.service.todo;
+
+import com.jiseok.todolist.web.dto.todo.CreateTodoReqDto;
+
+public interface TodoService {
+	
+	// 추가
+	public boolean createTodo(CreateTodoReqDto createTodoReqDto) throws Exception;
+	
+	// 수정
+	
+	// 삭제
+	
+	// 조회
+	
+}
---
+++ src/main/java/com/jiseok/todolist/service/todo/TodoServiceImpl.java
@@ -0,0 +1,23 @@
+package com.jiseok.todolist.service.todo;
+
+import org.springframework.stereotype.Service;
+
+import com.jiseok.todolist.domain.todo.Todo;
+import com.jiseok.todolist.domain.todo.TodoRepository;
+import com.jiseok.todolist.web.dto.todo.CreateTodoReqDto;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TodoServiceImpl implements TodoService {
+	
+	private final TodoRepository todoRepository;
+
+	@Override
+	public boolean createTodo(CreateTodoReqDto createTodoReqDto) throws Exception {
+		Todo todoEntity = createTodoReqDto.toEntity();
+		return todoRepository.save(todoEntity) > 0;
+	}
+
+}
