f1f4b46b43c3139219341a612ead3b114e4c1c23
Hanjiseok1 <heemoon25@naver.com>
2022-08-02 21:47:35 +0900

    scroll study

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,5 +1,24 @@
 const selectedTypeButton = document.querySelector(".selected-type-button");
 const typeSelectBoxList = document.querySelector(".type-select-box-list");
+const typeSelectBoxListLis = typeSelectBoxList.querySelectorAll("li");
+const todoContentList = document.querySelector(".todo-content-list");
+const sectionBoby = document.querySelector(".section-body");
+
+let page = 1;
+let totalPage = 0;
+
+sectionBoby.onscroll = () => {
+	let checkNum = todoContentList.clientHeight - sectionBoby.offsetHeight - sectionBoby.scrollTop;
+	
+	if(checkNum < 1 && checkNum > -1 && page < totalPage){
+		console.log(page);
+		console.log(totalPage);
+		page++;
+		load();
+	}
+	
+	
+}
 
 let listType = "all";
 
@@ -9,30 +28,60 @@ selectedTypeButton.onclick = () => {
     typeSelectBoxList.classList.toggle("visible");
 }
 
-selectedTypeButton.onblur = () => {
-    typeSelectBoxList.classList.toggle("visible");
+
+for(let i = 0; i < typeSelectBoxListLis.length; i++){
+	
+	typeSelectBoxListLis[i].onclick = () => {
+		page = 1;
+		
+		for(let i = 0; i < typeSelectBoxListLis.length; i++){
+			typeSelectBoxListLis[i].classList.remove("type-selected");
+		}
+		
+		const selectedType = document.querySelector(".selected-type");
+		
+		typeSelectBoxListLis[i].classList.add("type-selected");
+		
+		listType = typeSelectBoxListLis[i].textContent.toLowerCase();
+		
+		selectedType.textContent = typeSelectBoxListLis[i].textContent;
+		
+		todoContentList.innerHTML = "";
+		
+		load();
+		
+		typeSelectBoxList.classList.toggle("visible");
+		
+	}
+	
 }
+
 
 function load() {
 	$.ajax({
 		type: "get",
 		url: `/api/v1/todolist/list/${listType}`,
 		data: {
-			page: 1,
+			"page": page,
 			contentCount: 20
 		},
 		dataType: "json",
 		success: (response) => {
-			console.log(JSON.stringify(response));
 			getList(response.data);
 		},
 		error: errorMessage
-	});
+		
+	})
+}
+
+function setTotalCount(totalCount){
+	totalPage = totalCount % 20 == 0 ? totalCount / 20 : Math.floor(totalCount / 20) + 1;
 }
 
 function getList(data) {
-	const todoContentList = document.querySelector(".todo-content-list");
-	
+	const incompleteCountNumber = document.querySelector(".incomplete-count-number");
+	incompleteCountNumber.textContent = data[0].incompleteCount;
+	setTotalCount(data[0].totalCount);
 	for(let content of data) {
 		const listContent = `
 			<li class="todo-content">
@@ -49,12 +98,12 @@ function getList(data) {
 	}
 }
 
+
 function errorMessage(request, status, error) {
 	alert("요청 실패");
 	console.log(request.status);
 	console.log(request.responseText);
 	console.log(error);
-	
 }
 
 
