7c58583ef323ce9de45c20264c5856a76eb9ce1e
Hanjiseok1 <heemoon25@naver.com>
2022-08-04 21:52:14 +0900

    todoList 클린코드 연습

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -5,58 +5,49 @@ const todoContentList = document.querySelector(".todo-content-list");
 const sectionBoby = document.querySelector(".section-body");
 const incompleteCountNumber = document.querySelector(".incomplete-count-number");
 
-let page = 1;
+/*
+	게시글 불러오기
+	
+	1. todoList의 type이 무엇인가 중요함.
+		- all
+		- importance
+		- complete
+		- incomplete
+	
+	2. 요청주소: /api/v1/todolist/list/{type}?page=페이지 번호&contentCount=게시글의 개수
+	3. AJAX 요청을 활용
+		- type: get
+		- url: 요청주소
+		- data: 객체형태 -> {key: value} -> {page: 1, contentCount: 20}
+		- dataType: 응답받을 데이터의 타입 -> json
+		- success: 함수 -> response 매개변수를 받아야함.
+		- error: 함수 -> 요청실패 400, 500
+				- 400 에러의 경우
+					- 매개변수 타입이 잘 못된 경우
+					- 요청 리소스가 잘 못된 경우
+					- 권한이 없어서 요청을 하지 못하는 경우
+					- contentType
+						- text/html
+						- text/plain
+						- application/json
+						- enctype: multipart/form-data
+				- 500 에러의 경우
+					- 가장 먼저 해야하는 것 > console창 에러의 가장 위쪽이면서 가장 오른쪽을 확인해야함
+					- 오타
+					- nullpointer
+					- sql
+					- indexOut
+					- di 잘 못 했을 때
+					- @Component(Contorller, RestContorller, Service, Mapper, Repository, Configuration) IoC에 등록 안했을 때
+					- Interface 겹칠 때, Bean 객체가 IoC에 여러개 생성되어 있을 때
+					
+*/
+
 let totalPage = 0;
-
-sectionBoby.onscroll = () => {
-	let checkNum = todoContentList.clientHeight - sectionBoby.offsetHeight - sectionBoby.scrollTop;
-	
-	if(checkNum < 1 && checkNum > -1 && page < totalPage){
-		console.log(page);
-		console.log(totalPage);
-		page++;
-		load();
-	}
-	
-	
-}
-
+let page = 1;
 let listType = "all";
 
 load();
-
-selectedTypeButton.onclick = () => {
-    typeSelectBoxList.classList.toggle("visible");
-}
-
-
-for(let i = 0; i < typeSelectBoxListLis.length; i++){
-	
-	typeSelectBoxListLis[i].onclick = () => {
-		page = 1;
-		
-		for(let i = 0; i < typeSelectBoxListLis.length; i++){
-			typeSelectBoxListLis[i].classList.remove("type-selected");
-		}
-		
-		const selectedType = document.querySelector(".selected-type");
-		
-		typeSelectBoxListLis[i].classList.add("type-selected");
-		
-		listType = typeSelectBoxListLis[i].textContent.toLowerCase();
-		
-		selectedType.textContent = typeSelectBoxListLis[i].textContent;
-		
-		todoContentList.innerHTML = "";
-		
-		load();
-		
-		typeSelectBoxList.classList.toggle("visible");
-		
-	}
-	
-}
-
 
 function load() {
 	$.ajax({
@@ -68,22 +59,152 @@ function load() {
 		},
 		dataType: "json",
 		success: (response) => {
-			getList(response.data);
+			const todoList = response.data;
+			
+			setTotalPage(todoList[0].totalCount);
+			setIncompleteCount(todoList[0].incompleteCount);
+			createList(todoList);
 		},
 		error: errorMessage
 		
 	})
 }
 
-function setTotalCount(totalCount){
+function setTotalPage(totalCount){
 	totalPage = totalCount % 20 == 0 ? totalCount / 20 : Math.floor(totalCount / 20) + 1;
 }
 
-function getList(data) {
+function setIncompleteCount(incompleteCount) {
+	incompleteCountNumber.textContent = incompleteCount;
+}
+
+function appendList(listContent) {
+	todoContentList.innerHTML += listContent
+}
+
+function addCompleteEvent(todoContent, todoCode) {
+	const completeCheck = todoContent.querySelector(".complete-check");
 	
-	incompleteCountNumber.textContent = data[0].incompleteCount;
-	setTotalCount(data[0].totalCount);
-	for(let content of data) {
+	completeCheck.onchange = () => {
+		let incompleteCount = parseInt(incompleteCountNumber.textContent);
+		
+		if(completeCheck.checked){
+			incompleteCountNumber.textContent = incompleteCount - 1;
+		}else{
+			incompleteCountNumber.textContent = incompleteCount + 1;
+		}
+		updateCheckStatus("complete", todoContent, todoCode);
+	}
+}
+
+function addImportanceEvent(todoContent, todoCode) {
+	const importanceCheck = todoContent.querySelector(".importance-check");
+	
+	importanceCheck.onchange = () => {
+		updateCheckStatus("importance", todoContent, todoCode);
+	}
+}
+
+function addDeleteEvent(todoContent, todoCode) {
+	const trashButton = todoContent.querySelector(".trash-button");
+	
+	trashButton.onclick = () => {
+		deleteTodo(todoContent, todoCode);
+	}
+}
+
+function addContentInputEvent(todoContent, todoCode) {
+	const todoContentText = todoContent.querySelector(".todo-content-text");
+	const todoContentInput = todoContent.querySelector(".todo-content-input");
+	let todoContentOldValue = null;
+	
+	let eventFlag = false;
+	
+	let updateTodo = () => {
+		const todoContentTNewValue = todoContentInput.value;
+		if(getChangeStatusOfValue(todoContentOldValue, todoContentTNewValue)){
+			if(updateTodoContent(todoCode, todoContentNetodoContentTNewValue)){
+				todoContentText.textContent = todoContentNewValue;
+			}
+		}
+		
+		todoContentText.classList.toggle("visible");
+		todoContentInput.classList.toggle("visible");
+	}
+	
+	todoContentText.onclick = () => {
+		todoContentValue = todoContentInput.value;
+		todoContentText.classList.toggle("visible");
+		todoContentInput.classList.toggle("visible");
+		todoContentInput.focus();
+		eventFlag = true;
+	}
+	
+	
+	
+	todoContentInput.onblur = () => {
+		if(eventFlag){
+			updateTodo();
+		}
+	}
+	
+	todoContentInput.onkeyup = () => {
+		if(window.event.keyCode == 13){
+			eventFlag = false;
+			updateTodo();
+		}
+	}
+}
+
+function getChangeStatusOfValue(originValue, newValue) {
+	return originValue != newValue;
+}
+
+function updateTodoContent(todoCode, todo) {
+	let successFlag = false;
+	$.ajax({
+		type: "put",
+		url: `/api/v1/todolist/todo/${todoCode}`,
+		contentType: "application/json",
+		data: JSON.stringify({
+			"todoCode": todoCode, 
+			"todo": todo
+			}),
+		async: false,
+		dataType: "json",
+		success: (response) => {
+			successFlag = response.data;
+		},
+		error: errorMessage
+	})
+	return successFlag;
+}
+
+function substringTodoCode(todoContent) {
+	const completeCheck = todoContent.querySelector(".complete-check");
+	
+	const todoCode = completeCheck.getAttribute("id");
+	const tokenIndex = todoCode.lastIndexOf("-");
+	
+	return todoCode.substring(tokenIndex + 1);
+}
+
+function addEvent() {
+	const todoContents = document.querySelectorAll(".todo-content");
+	
+	for(let todoContent of todoContents){
+		const todoCode = substringTodoCode(todoContent);
+		
+		addCompleteEvent(todoContent, todoCode);
+		addImportanceEvent(todoContent, todoCode);
+		addDeleteEvent(todoContent, todoCode);
+		addContentInputEvent(todoContent, todoCode);
+	}
+}
+
+function createList(todoList) {
+	
+	for(let content of todoList) {
 		const listContent = `
 			<li class="todo-content">
                 <input type="checkbox" id="complete-check-${content.todoCode}" class="complete-check" ${content.todoComplete ? 'checked' : ''}>
@@ -95,90 +216,70 @@ function getList(data) {
                 <div class="trash-button"><i class="fa-solid fa-trash"></i></div>
             </li>
 		`
-		todoContentList.innerHTML += listContent;
+		appendList(listContent);
 	}
+	
 	addEvent();
 }
 
-function addEvent() {
-	const todoContents = document.querySelectorAll(".todo-content");
+sectionBoby.onscroll = () => {
+	console.log(sectionBoby.scrollTop)
+	let checkNum = todoContentList.clientHeight - sectionBoby.offsetHeight - sectionBoby.scrollTop;
 	
-	for(let i = 0; i < todoContents.length; i++) {
-		let todoCode = todoContents[i].querySelector(".complete-check").getAttribute("id");
-		let index = todoCode.lastIndexOf("-");
-		todoCode = todoCode.substring(index + 1);
-		
-		
-		todoContents[i].querySelector(".complete-check").onchange = () => {
-			let incompleteCount = parseInt(incompleteCountNumber.textContent);
-			
-			if(todoContents[i].querySelector(".complete-check").checked){
-				incompleteCountNumber.textContent = incompleteCount - 1;
-			}else {
-				incompleteCountNumber.textContent = incompleteCount + 1;
-			}
-			updateCheckStatus("complete", todoContents[i], todoCode);
-		}
-		
-		todoContents[i].querySelector(".importance-check").onchange = () => {
-			updateCheckStatus("importance", todoContents[i], todoCode);
-		}
-		
-		todoContents[i].querySelector(".trash-button").onclick = () => {
-			deleteTodo(todoContents[i], todoCode);
-		}
-		
-		const todoContentText = todoContents[i].querySelector(".todo-content-text");
-		const todoContentInput = todoContents[i].querySelector(".todo-content-input");
-		let todoContentValue = null;
-		
-		let eventFlag = false;
-		
-		todoContentText.onclick = () => {
-			todoContentValue = todoContentInput.value;
-			todoContentText.classList.toggle("visible");
-			todoContentInput.classList.toggle("visible");
-			todoContentInput.focus();
-			eventFlag = true;
-		}
-		
-		let updateTodoContent = () => {
-			if(todoContentValue != todoContentInput.value) {
-				$.ajax({
-					type: "put",
-					url: `/api/v1/todolist/todo/${todoCode}`,
-					contentType: "application/json",
-					data: JSON.stringify({
-						"todoCode": todoCode,
-						todo: todoContentInput.value
-						}),
-					async: false,
-					dataType: "json",
-					success: (response) => {
-						if(response.data) {
-							todoContentText.textContent = todoContentInput.value;
-						}
-					},
-					error: errorMessage
-				})
-			}	
-			todoContentText.classList.toggle("visible");
-			todoContentInput.classList.toggle("visible");	
-		}
-		
-		todoContentInput.onblur = () => {
-			if(eventFlag) {
-				updateTodoContent();
-			}
-		}
-		todoContentInput.onkeyup = () => {
-			if(window.event.keyCode == 13) {
-				eventFlag = false;
-				updateTodoContent();
-			}
-			
-		}
+	if(checkNum < 1 && checkNum > -1 && page < totalPage){
+		console.log(page);
+		console.log(totalPage);
+		page++;
+		load();
 	}
+}
+
+
+selectedTypeButton.onclick = () => {
+    typeSelectBoxList.classList.toggle("visible");
+}
+
+function resetPage() {
+	page = 1;
+}
+
+function removeAllclassList(elements, className) {
+	for(let element of elements){
+		elements.classList.remove(className);
+	}
+}
+
+function setListType(selectedType) {
+	listType = selectedType.toLowerCase();
+}
+
+function clearTodoContentList() {
+		todoContentList.innerHTML = "";
+}
+
+for(let i = 0; i < typeSelectBoxListLis.length; i++){
+	
+	typeSelectBoxListLis[i].onclick = () => {
+		resetPage();
+
+		removeAllclassList(typeSelectBoxListLis, "type-selected");
+		
+		typeSelectBoxListLis[i].classList.add("type-selected");
+		
+		setListType(typeSelectBoxListLis[i].textContent);
+		
+		const selectedType = document.querySelector(".selected-type");
+		
+		selectedType.textContent = typeSelectBoxListLis[i].textContent;
+	
+		clearTodoContentList();
+		
+		load();
+		
+		typeSelectBoxList.classList.toggle("visible");
+		
+	}
+	
 }
 
 function updateStatus(type, todoCode) {
@@ -187,11 +288,11 @@ function updateStatus(type, todoCode) {
 	$.ajax({
 		type: "put",
 		url: `/api/v1/todolist/${type}/todo/${todoCode}`,
-		async: false,  //동기처리
+		async: false,
 		dataType: "json",
 		success: (response) => {
-			result = response.data == true
-				
+			result = response.data
+			
 		},
 		error: errorMessage
 	})
@@ -201,14 +302,18 @@ function updateStatus(type, todoCode) {
 function updateCheckStatus(type, todoContent, todoCode) {
 	let result = updateStatus(type, todoCode);
 	
-	if((
-		(type == "complete"
+	if(
+			(
+				(
+					type == "complete" 
+					&& 
+					(listType == "complete" || listType == "incomplete")
+				) 
+				|| 
+				(type == "importance" && listType == "importance")
+			) 
 			&& 
-		(listType == "complete" || listType == "incomplete"))
-		|| 
-			(type == "importance" && listType == "importance"))
-		&&	
-		result
+			result
 		) {
 		todoContentList.removeChild(todoContent);
 	}
@@ -221,13 +326,12 @@ function deleteTodo(todoContent, todoCode) {
 		async: false,
 		dataType: "json",
 		success: (response) => {
-			if(response.data) {
+			if(response.data){
 				todoContentList.removeChild(todoContent);
 			}
 		},
 		error: errorMessage
 	})
-
 }
 
 function errorMessage(request, status, error) {
